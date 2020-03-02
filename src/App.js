eaf91812f3ad377279007f82e51c4e182a6631fb
TomasTheHuman <fraymark59@gmail.com>
2020-03-02 17:36:08 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if(newItem.text !== ""){
+      const newItems = [...this.state.items, newItem];
+      this.setState({
+        items:newItems,
+        currentItem:{
+          text:'',
+          key:''
+        }
+      })
+    }
+  }
+
+  deleteItem(key){
+    const filteredItems = this.state.items.filter(item => 
+      item.key !== key);
+      this.setState({
+        items:filteredItems
+      })
+  }
+
+  render(){
+    return(
+    <div>
+    <header>
+      <form id="todoform" onSubmit={this.addItem}>
+        <input type="text" placeholder = "Введите текст"
+        value = {this.state.currentItem.text}
+        onChange={this.handleInput}/>
+        <button type="submit">Add</button>
+      </form>
+    </header>
+    <ListItems items = {this.state.items}
+    deleteItem = {this.deleteItem}
+    ></ListItems>
     </div>
-  );
+    );
+  }
+}
+
+function ListItems(props){
+  const items = props.items;
+  const listItems = items.map(item =>{
+      return <div className="list" key={item.key}>
+          <p>{item.text}
+          <button id="butt" onClick={ () => props.deleteItem(item.key) }>Del</button>
+          </p>
+          
+      </div>
+  });
+  return(
+      <div>{listItems}</div>
+  )
 }
 
 export default App;
